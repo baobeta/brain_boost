288d5004652687695314d194e0368a2af2595458
baolequoc <bao.lq@holistics.io>
2024-05-06 23:24:34 +0700

    feat: add floating vue for project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import './index.css'
-
+import 'floating-vue/dist/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import FloatingVue from 'floating-vue'
 
 import App from './App.vue'
 import router from './router'
 
 
+
 const app = createApp(App)
 
+app.use(FloatingVue)
 app.use(createPinia())
 app.use(router)
 
