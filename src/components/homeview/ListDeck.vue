f41f6cf9c9fe2ae356ca766a6d37a2a6170b18cb
baolequoc <bao.lq@holistics.io>
2024-05-06 22:48:46 +0700

    feat: add list desk field

---
+++ src/components/homeview/ListDeck.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="flex bg-gray-200 text-gray-900 items-center py-2 px-4 rounded max-w-2xl text-xl justify-between mt-2">
+    <span class="font-base">{{ title }}</span>
+    <FontAwesomeIcon :icon="faAngleRight"/>
+  </div>
+</template>
+
