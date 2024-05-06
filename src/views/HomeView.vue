6ab0eee01fc3f5d2e2984b80de80c79d4a708469
baolequoc <bao.lq@holistics.io>
2024-05-06 22:59:10 +0700

    feat: add homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import ButtonIcon from '@/components/core/ButtonIcon.vue';
+import ListDeck from '@/components/homeview/ListDeck.vue';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <template>
-  <div class="home text-3xl text-red-500">
-    Hello world
-  </div>
+  <section class="home flex flex-col items-center">
+    <div class="flex flex-col w-3/4 mt-2">
+      <div class="flex justify-end">
+        <ButtonIcon :title="'New Deck'" :icon="faPlus" />
+      </div>
+      <ListDeck :title="'List Deck'"/>
+      <ListDeck :title="'List Deck'"/>
+    </div>
+  </section>
 </template>
