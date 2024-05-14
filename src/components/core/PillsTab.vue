8863fb870b9ca74f017dcaf5c52b256efa6c9951
baolequoc <bao.lq@holistics.io>
2024-05-14 23:26:49 +0700

    feat: add pill tabs

---
+++ src/components/core/PillsTab.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface Props {
+  options: string[];
+}
+
+const selected = defineModel();
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: "",
+  options: () => ["one", "two", "tree"],
+});
+</script>
+<template>
+  <ul class="flex text-sm font-medium text-center text-gray-500 rounded border py-4">
+    <li class="flex grow" v-for="option in options">
+      <a
+        href="#"
+        class="px-4 py-2 rounded-lg border grow mx-2"
+        @click="selected = option"
+        :class="{ active: selected === option }"
+        >{{ option }}</a
+      >
+    </li>
+  </ul>
+</template>
--- src/views/StatisticsPage.vue
+++ src/views/StatisticsPage.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import ProSelect from "../components/core/ProSelect.vue";
+import PillsTab from "../components/core/PillsTab.vue";
 </script>
 <template>
   <section>
-    <ProSelect />
     <span class="text-slate-500">Showing Statistics of</span>
+    <ProSelect />
+    <PillsTab class="mt-4" />
   </section>
 </template>
