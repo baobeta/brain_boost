6cb286b2f96952550f4393c34fde76c862ccea0a
baolequoc <bao.lq@holistics.io>
2024-06-22 14:11:08 +0700

    change: add tooltip for add desk

--- src/components/homeview/ListDeck.vue
+++ src/components/homeview/ListDeck.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 defineProps<{
   title: string
@@ -10,7 +8,16 @@ defineProps<{
 <template>
   <div class="flex bg-gray-100 text-gray-800 items-center py-2 px-4 rounded max-w-2xl text-xl justify-between mt-2 hover:bg-gray-300">
     <span class="font-base">{{ title }}</span>
-    <FontAwesomeIcon :icon="faAngleRight"/>
+    <VDropdown>
+      <vue-feather type="more-horizontal" class="w-6 h-6" />
+      <template #popper>
+        <div class="flex flex-col">
+          <div class>
+            <button class="text-blue-500 hover:text-blue-600">Add sub desk</button>
+          </div>
+        </div>
+      </template>
+    </VDropdown>
   </div>
 </template>
 
