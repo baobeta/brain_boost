2f310c696f9ef069a4a8e6a2705245435aeeaa80
baobeta <lequocbao29072001@gmail.com>
2024-10-19 14:56:14 +0700

    chore: setup go repo backend

---
+++ server/pkg/backend/backend.go
@@ -0,0 +1,10 @@
+package backend
+
+// func main() {
+//   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hello from Go backend!")
+//   })
+
+//   log.Println("Backend running on http://localhost:8080")
+//   log.Fatal(http.ListenAndServe(":8080", nil))
+// }
