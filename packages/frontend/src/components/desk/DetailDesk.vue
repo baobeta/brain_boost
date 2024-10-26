b8f96c092aab123eacad7c74c0502e4fa0336d54
baolequoc <bao.lq@holistics.io>
2024-10-26 16:39:22 +0700

    change: refactor layout for card

--- packages/frontend/src/components/desk/DetailDesk.vue
+++ packages/frontend/src/components/desk/DetailDesk.vue
@@ -4,10 +4,11 @@ import { useDesk } from '@/service/useDesk';
 import { useCard } from '@/service/useCard';
 import { ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import SummaryCard from './SummaryCard.vue';
+
 import CardElement from './CardElement.vue';
 import type { ICard } from '@/models/card';
 import { useToast } from 'primevue/usetoast';
+import PanelSummary from './PanelSummary.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -64,12 +65,7 @@ async function handleEditCard(id: string) {
           <div class="font-semibold text-xl">{{ deskData.name }}</div>
           <PButton label="Add Cards" outlined severity="contrast" icon="pi pi-plus" @click="handleAddCard" />
         </div>
-        <div class="grid grid-cols-3 gap-4 mb-6 mt-2">
-          <SummaryCard icon="sparkles" title="New" :total="0" :color="'purple'" />
-          <SummaryCard icon="arrow-circle-right" title="Learning" :total="0" color="yellow" />
-          <SummaryCard icon="book" title="Review" :total="0" color="blue" />
-        </div>
-        <PButton label="Learn" />
+        <PanelSummary class="mt-4" />
         <div class="flex justify-between items-center mt-6 mb-4">
           <div dir="ltr" data-orientation="horizontal">
             <div class="flex space-x-4">
@@ -78,15 +74,10 @@ async function handleEditCard(id: string) {
             </div>
           </div>
           <div class="flex space-x-2">
-            <button
-              class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
-            >
-              Custom
-            </button>
             <PButton icon="pi pi-ellipsis-v" outlined severity="contrast" />
           </div>
         </div>
-        <div class="flex flex-col space-y-2">
+        <div class="flex flex-col space-y-2 overflow-auto" style="max-height: calc(100vh - 530px)">
           <CardElement
             v-for="card in listCards"
             id="test"
---
+++ packages/frontend/src/components/desk/PanelSummary.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import Panel from 'primevue/panel';
+import SummaryCard from './SummaryCard.vue';
+</script>
+<template>
+  <Panel>
+    <div class="flex flex-col">
+      <div class="flex justify-between space-x-2 p-10">
+        <SummaryCard class="w-1/3" icon="sparkles" title="New" :total="0" :color="'purple'" />
+        <SummaryCard class="w-1/3" icon="arrow-circle-right" title="Learning" :total="0" color="yellow" />
+        <SummaryCard class="w-1/3" icon="book" title="Review" :total="0" color="blue" />
+      </div>
+
+      <PButton label="Learn" class="mt-2" />
+    </div>
+  </Panel>
+</template>
