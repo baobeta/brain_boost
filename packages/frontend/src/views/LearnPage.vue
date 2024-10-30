477d27893f681e9133aecf4faf41cfb4ff9128d6
baolequoc <bao.lq@holistics.io>
2024-10-30 23:19:04 +0700

    change: fetch card form db

--- packages/frontend/src/views/LearnPage.vue
+++ packages/frontend/src/views/LearnPage.vue
@@ -2,12 +2,39 @@
   <div>{{ $route.params.deskId }}</div>
 </template>
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { useCard } from '@/service/useCard';
+import { computed, onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { State } from 'ts-fsrs';
+import type { ICard } from '@/models/card';
 const route = useRoute();
 const deskId = computed(() => route.params.deskId);
 const reviewingCards = ref([]);
 const reviewedCards = ref([]);
-
+const { fetchCards } = useCard();
 const today = new Date(Date.now());
+
+const providedCards = ref<ICard[]>([]);
+
+const critialCards = computed(() => {
+  return providedCards.value
+    .filter((card: ICard) => [State.Learning, State.Relearning].includes(card.model.state))
+    .sort((a: ICard, b: ICard) => (a.model.due as Date).getTime() - (b.model.due as Date).getTime());
+});
+
+const newCards = computed(() => {
+  return providedCards.value
+    .filter((card: ICard) => card.model.state === State.New)
+    .sort((a: ICard, b: ICard) => (a.model.due as Date).getTime() - (b.model.due as Date).getTime());
+});
+
+const toReviewCards = computed(() => {
+  return providedCards.value
+    .filter((card: ICard) => card.model.state === State.Review && card.model.due < today)
+    .sort((a: ICard, b: ICard) => (a.model.due as Date).getTime() - (b.model.due as Date).getTime());
+});
+
+onMounted(async () => {
+  providedCards.value = await fetchCards();
+});
 </script>
