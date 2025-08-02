2f9f1b97110a3e1ebe3c5cebcb56044543b62db7
baolequoc <bao.lq@holistics.io>
2025-08-02 14:26:02 +0700

    refactor: clean up project used new version

--- packages/frontend/src/components/footer/FooterView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <footer class="border-t-2 flex justify-center">
-    <p>This is new page by Bao Le</p>
-  </footer>
-</template>
-<script lang="ts" setup> </script>
--- packages/frontend/src/components/sidebar/SidebarLogo.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-const imgLink = 'https://i.imgur.com/qHivSnr.jpeg'
-</script>
-<template>
-  <section class="flex items-center justify-start p-2 mx-2 mt-2">
-    <img :src="imgLink" class="w-8 h-8"/>
-    <span class="ml-2 font-semibold">BrainBoost</span>
-  </section>
-</template>
